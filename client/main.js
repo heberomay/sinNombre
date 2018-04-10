0c64ec8bcecb6e4c10013ccd46804f069fb35490
heberomay <heberomay16@gmail.com>
2018-04-10 14:44:29 -0300

    nuevo api key

--- client/main.js
+++ client/main.js
@@ -1,3 +1,3 @@
 Meteor.startup (function(){
-	GoogleMaps.load({ v: '3', key: 'AIzaSyA5uLBlnGLuP9xvKKyuEj2WqHCOzbdqYio', libraries: 'geometry,places' });
+	GoogleMaps.load({ v: '3', key: 'AIzaSyAVXkhf4qAAtxRoK846NhCd-s5rOs4zyN0' , libraries: 'geometry,places' });
 });
